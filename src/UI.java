0e8457abdf733783883107f9883e59f887c3577f
banulaperera <banulalakidu59@gmail.com>
2022-12-11 13:54:24 +0530

    Initial Commit

--- src/UI.java
+++ src/UI.java
@@ -4,18 +4,31 @@ import java.awt.*;
 
 public class UI extends JFrame{
     public UI(){
+        //Adding background image to the frame
+        JLabel background = new JLabel(new ImageIcon("UOW.jpg"));
+        background.setLayout(new FlowLayout());
+
+        //adding welcome letter to the JFrame
+        JLabel welcomeLetter = new JLabel("Welcome to the Westminster Skin Consultation Manager");
+        welcomeLetter.setForeground(Color.YELLOW);
+        welcomeLetter.setFont(new Font("Calibri", Font.BOLD,40));
+        welcomeLetter.setBackground(Color.BLACK);
+        welcomeLetter.setOpaque(true);
+        background.add(welcomeLetter);
+
+        //Creating border with color yellow
+        Border border = BorderFactory.createLineBorder(Color.WHITE,5);
+        background.setBorder(border);
+
+        //Adding buttons to the login page
+        JButton button1 = new JButton();
+
+
         setTitle("Westminster Skin Consultation Manager");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(1500, 800);
-        setVisible(true);
-
-        //Adding background image to the frame
         setLayout(new BorderLayout());
-        JLabel background = new JLabel(new ImageIcon("UOW.jpg"));
+        setVisible(true);
         add(background);
-        background.setLayout(new FlowLayout());
-
-        Border border = BorderFactory.createLineBorder(Color.WHITE,5); //Creating border with color yellow
-        background.setBorder(border);
     }
 }
